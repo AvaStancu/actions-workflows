7f06f41e757e62c246b77c5b7c3c779e5448bb86
Ava Stancu <avastancu@github.com>
2023-01-11 15:50:29 +0200

    Create run_arc_jobs.go

---
+++ test/e2e/run_arc_jobs.go
@@ -0,0 +1,19 @@
+import ("flag", "testing")
+
+import  "golang.org/x/exp/slices"
+
+
+func TestARCJobs(t *testing.T){
+    availablePods := flag.String("pods")
+    flag.Parse()
+
+    expectedPods = []string{"listener", "runner", "controller-manager"}
+
+    t.Run("Get available pods after job run", func(t *testing.T) {
+      for _, podName in range := expectedPods {
+          if !slices.Contains(*availablePods, podName) {
+              t.Fatalf("%v pod not found.", podName)
+            }
+        }
+    }
+}
