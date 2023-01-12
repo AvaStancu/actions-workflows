de78cd57bdb2069bad0f5b81d92e448fe203114f
Ava Stancu <avastancu@github.com>
2023-01-12 13:55:20 +0200

    Update run_arc_jobs_test.go

--- test/e2e_dev_arc/run_arc_jobs_test.go
+++ test/e2e_dev_arc/run_arc_jobs_test.go
@@ -2,6 +2,7 @@ package e2e_dev_arc
 
 import (
 	"flag"
+	"fmt"
 	"testing"
 
 	"golang.org/x/exp/slices"
@@ -14,6 +15,7 @@ func TestARCJobs(t *testing.T) {
 	expectedPods := []string{"listener", "runner", "controller-manager"}
 
 	t.Run("Get available pods after job run", func(t *testing.T) {
+		fmt.Printf("Here we go")
 		for _, podName := range expectedPods {
 			if !slices.Contains(*availablePods, podName) {
 				t.Fatalf("%v pod not found.", podName)
